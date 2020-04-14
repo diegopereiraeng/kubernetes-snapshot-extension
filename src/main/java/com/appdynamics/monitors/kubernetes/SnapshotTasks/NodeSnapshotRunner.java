7ecb63a5a1574506cd8aa14414c3245e48b22fd7
Diego Pereira <diegopereiraeng@gmail.com>
2020-04-14 14:58:47 -0300

    Nodes Count + Namespaces Count + PodRestarts fix to diff from the last run + PodRestartsSum metric

--- src/main/java/com/appdynamics/monitors/kubernetes/SnapshotTasks/NodeSnapshotRunner.java
+++ src/main/java/com/appdynamics/monitors/kubernetes/SnapshotTasks/NodeSnapshotRunner.java
@@ -73,7 +73,7 @@ public class NodeSnapshotRunner extends SnapshotRunnerBase {
                 catch (Exception ex){
                     throw new Exception("Unable to connect to Kubernetes API server because it may be unavailable or the cluster credentials are invalid", ex);
                 }
-                logger.info("Analyzing Nodes - Number of nodes: "+ nodeList.getItems().size());
+                logger.debug("Analyzing Nodes - Number of nodes: "+ nodeList.getItems().size());
                 createNodePayload(nodeList, config, publishUrl, accountName, apiKey);
 
                 //build and update metrics
@@ -103,7 +103,7 @@ public class NodeSnapshotRunner extends SnapshotRunnerBase {
             String nodeName = nodeObj.getMetadata().getName();
             nodeObject = checkAddObject(nodeObject, nodeName, "nodeName");
 
-            logger.info("Node Name Check: "+nodeName);
+            logger.debug("Node Name Check: "+nodeName);
 
             String clusterName = Utilities.ensureClusterName(config, nodeObj.getMetadata().getClusterName());
 
@@ -111,6 +111,14 @@ public class NodeSnapshotRunner extends SnapshotRunnerBase {
             if (summary == null) {
                 summary = initNodeSummaryObject(config, ALL);
                 getSummaryMap().put(ALL, summary);
+            }
+
+            SummaryObj summaryNode = getSummaryMap().get(nodeName);
+            if(Utilities.shouldCollectMetricsForNode(getConfiguration(), nodeName)) {
+                if (summaryNode == null) {
+                    summaryNode = initNodeSummaryObject(config, nodeName);
+                    getSummaryMap().put(nodeName, summaryNode);
+                }
             }
 
             SummaryObj summaryWorker = getSummaryMap().get("Workers");
@@ -125,13 +133,7 @@ public class NodeSnapshotRunner extends SnapshotRunnerBase {
                 getSummaryMap().put("Masters", summaryMaster);
             }
 
-            SummaryObj summaryNode = getSummaryMap().get(nodeName);
-            if(Utilities.shouldCollectMetricsForNode(getConfiguration(), nodeName)) {
-                if (summaryNode == null) {
-                    summaryNode = initNodeSummaryObject(config, nodeName);
-                    getSummaryMap().put(nodeName, summaryNode);
-                }
-            }
+            
 
 
             nodeObject = checkAddObject(nodeObject, clusterName, "clusterName");
