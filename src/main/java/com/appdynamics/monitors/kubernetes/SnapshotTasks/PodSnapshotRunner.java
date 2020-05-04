9bc6ad7821fe9b1d68fdd177abd178df17fe35d1
Diego Pereira <diegopereiraeng@gmail.com>
2020-05-04 13:56:45 -0300

    metricpath fix

--- src/main/java/com/appdynamics/monitors/kubernetes/SnapshotTasks/PodSnapshotRunner.java
+++ src/main/java/com/appdynamics/monitors/kubernetes/SnapshotTasks/PodSnapshotRunner.java
@@ -724,7 +724,7 @@ public class PodSnapshotRunner extends SnapshotRunnerBase {
 
         ArrayList<AppDMetricObj> metricsList = initMetrics(config, namespace, node);
         String path = Utilities.getMetricsPath(config, namespace, node);
-
+        logger.info("Init Pod Path: %s",path);
         return new SummaryObj(summary, metricsList, path);
     }
 
